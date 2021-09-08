d27b0a78a54f7e11f7ba79a84a672b7f83c7cb01
Florian Windolf <florian@presize.ai>
2021-09-08 16:47:21 +0200

    Setup tests

---
+++ test/exercise_test.dart
@@ -0,0 +1,19 @@
+import 'package:test/test.dart';
+
+import 'package:stronk/models/exercise/exercise.dart';
+
+// class MockExerciseController extends Mock implements BaseExerciseController {}
+
+void main() {
+  group('Exercise', () {
+    test("can_be_deserialized_and_serialized", () {
+      final exercise = Exercise.empty();
+
+      expect(exercise, Exercise.fromJson(exercise.toJson()));
+    });
+
+    test("can_create_new_exercise", () {
+      final mockStateNotifier = null;
+    });
+  });
+}
